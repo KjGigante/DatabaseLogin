7e91a20fa1dd25785fdfeaa53b3f54d77d13253a
Kevin Jeiel Gigante <gigante.kevinjeiel@ue.edu.ph>
2020-10-13 11:47:13 +0800

    updated java validation

--- val.js
+++ val.js
@@ -17,7 +17,6 @@ function myFunction(){
 	var userValue = document.getElementById('user').value;
 	var passValue = document.getElementById('pass').value;
 	var passrptValue = document.getElementById('passrpt').value;
-	alert('line 15');
 	var letters = /^[A-Za-z]*$/;
 	var lettersSpace = /^[A-Za-z\s]*$/;
     var numbers = /^[-+]?[0-9]+$/;
@@ -26,10 +25,36 @@ function myFunction(){
     var unameformat = /^[a-zA-Z-_]*$/;
     var mnlen = 1;
     var mxlen = 2;
+    var alphaNum = /^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$/i;
 
-    if (lNameValue=="") {
-    	alert('Test');
+    if (!lNameValue.match(lettersSpace)) {
+    	alert('Error: Last name. Only alphabets are allowed');
+    	return false;
     }
-    alert('Test 2');
-    return false;
+    if (!fNameValue.match(lettersSpace)) {
+    	alert('Error: First name. Only alphabets are allowed');
+    	return false;
+    }
+    if (!mNameValue.match(letters)) {
+    	alert('Error: Middle inital. Only alphabets are allowed');
+    	return false;
+    }
+    if (mNameValue.length>2){
+    	alert('Error: Middle inital. Maximum of two characters');
+    	return false;
+    }
+    if (studnoValue.length>11||studnoValue.length<11) {
+    	alert('Error: Student number. Must be 11 characters');
+    	alert(bdayValue);
+    	return false;
+    }
+    if (!studnoValue.match(numbers)){
+    	alert('Error: Student number. Only numbers are allowed');
+    	return false;
+    }
+    
+    
+    
+
 }
+    
