182052747969ab8a6e4e6b8edc710af99b8861aa
Kevin Jeiel Gigante <gigante.kevinjeiel@ue.edu.ph>
2020-10-13 12:22:57 +0800

    Final validation for java

--- val.js
+++ val.js
@@ -19,13 +19,14 @@ function myFunction(){
 	var passrptValue = document.getElementById('passrpt').value;
 	var letters = /^[A-Za-z]*$/;
 	var lettersSpace = /^[A-Za-z\s]*$/;
-    var numbers = /^[-+]?[0-9]+$/;
+    var numbers = /^[0-9]+$/;
     var letterNumber = /^[0-9a-zA-Z]+$/;
     var mailformat = /^[a-z0-9.]+[@ue.edu.ph]*$/;
     var unameformat = /^[a-zA-Z-_]*$/;
     var mnlen = 1;
     var mxlen = 2;
     var alphaNum = /^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$/i;
+    var newBdayValue = bdayValue.replace(/-/, "");
 
     if (!lNameValue.match(lettersSpace)) {
     	alert('Error: Last name. Only alphabets are allowed');
@@ -52,9 +53,41 @@ function myFunction(){
     	alert('Error: Student number. Only numbers are allowed');
     	return false;
     }
+    if (newBdayValue > "20021010") {
+    	alert('Error: Birthday. Must be at least 18 years old')
+    	return false;
+    }
+    if (!phoneValue.match(numbers)) {
+    	alert('Error: Mobile number. Only numbers are allowed')
+    	return false;
+    }
+    if (phoneValue.length !=10) {
+    	alert('Error: Mobile number. Format +63XXXXXXXXXX. Input 10 numbers');
+    	return false;
+    }
+    if (!emailValue.match(mailformat)) {
+    	alert('Error: Email. Must be a valid UE email');
+    	return false;
+    }
+    if (!userValue.match(unameformat)) {
+    	alert('Error: Username. Only alphabets, dash(-) or underscore(_)');
+    	return false;
+    }
+    if (userValue.length < 8 || userValue.length >15) {
+    	alert('Error: Username. 8-15 characters only')
+    	return false;
+    }
+    if (!passValue.match(alphaNum)) {
+    	alert('Error: Password. Only accepts cominations of alphanumeric');
+    	return false;
+    }
+    if (passValue.length < 8 || passValue.length > 20 ) {
+    	alert('Error: Password. 8-15 characters only');
+    	return false;
+    }
+
     
-    
-    
+    return true;
 
 }
     
