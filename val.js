752bef30197886d4df1c2dbd84362ca6b8c48945
Kevin Jeiel Gigante <gigante.kevinjeiel@ue.edu.ph>
2020-10-13 10:35:11 +0800

    modified js for html

--- val.js
+++ val.js
@@ -1,26 +1,35 @@
-function validateAll(){
-	
-	if (ValidateEmail){
-		if (checkCheckBox){
-			document.form.submit();
-		}
-	}
+function newFunction(){
+	alert('Help');
+	return false;
 }
-function ValidateEmail(mail) 
-{
- if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.email))
-  {
-    return (true)
-  }else
-    alert("You have entered an invalid email address!")
-    return (false)
+function test(){
+	alert ('line2');
+
 }
-function checkCheckBox{
-	var x = document.getElementById("termsBox").checked;
-	if (x === true){
-		return true
-	}
-	else{
-		return false
-	}
+function myFunction(){
+	var lNameValue = document.getElementById('lName').value;
+	var fNameValue = document.getElementById('fName').value;
+	var mNameValue = document.getElementById('mName').value;
+	var studnoValue = document.getElementById('studno').value;
+	var bdayValue = document.getElementById('bday').value;
+	var phoneValue = document.getElementById('phone').value;
+	var emailValue = document.getElementById('email').value;
+	var userValue = document.getElementById('user').value;
+	var passValue = document.getElementById('pass').value;
+	var passrptValue = document.getElementById('passrpt').value;
+	alert('line 15');
+	var letters = /^[A-Za-z]*$/;
+	var lettersSpace = /^[A-Za-z\s]*$/;
+    var numbers = /^[-+]?[0-9]+$/;
+    var letterNumber = /^[0-9a-zA-Z]+$/;
+    var mailformat = /^[a-z0-9.]+[@ue.edu.ph]*$/;
+    var unameformat = /^[a-zA-Z-_]*$/;
+    var mnlen = 1;
+    var mxlen = 2;
+
+    if (lNameValue=="") {
+    	alert('Test');
+    }
+    alert('Test 2');
+    return false;
 }
