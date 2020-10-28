cc048d5d91dca0fde6d6f7f566a279a1cfedae35
Kevin Jeiel Gigante <gigante.kevinjeiel@ue.edu.ph>
2020-10-28 23:59:53 +0800

    success in mod 7 validation

---
+++ newTester.js
@@ -0,0 +1,8 @@
+function test(){
+	var lNameValue = document.getElementById('lName').value;
+	if (lNameValue=="") {
+    	alert('iz null');
+    }
+	alert ('line2');
+	return false;
+}
---
+++ newmplogin.php
@@ -0,0 +1,28 @@
+<?php //dabatabse
+require "dbConnect.php";
+ if(isset($_POST['logsubmit'])){
+ 	$username 		= $_POST['user'];
+ 	$password 		= $_POST['pass'];
+ 	
+ 	$con = config::connect();
+ 	$query = "SELECT * FROM users WHERE uidUsers = :username AND pwdUsers = :password";
+ 	$statement = $con->prepare($query);
+ 	$statement->execute(
+ 		array(
+ 			'username' => $_POST['user'],
+ 			'password' => $_POST['pass']
+ 		)
+ 	);
+ 	$count = $statement->rowCount();
+ 	if($count > 0){
+ 		header('location:newsuccuss.html');
+ 	}else
+ 	{
+ 		echo '<script>
+				alert("Wrong credentials. Please try again.");
+				window.history.go(-1);
+			</script>';
+ 	}
+ }
+
+?>
